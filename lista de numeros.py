e473680d7b197d625c14ec614b20c99a3f7caff3
alexfe <felipe7771103@gmail.com>
2025-05-03 09:59:50 -0500

    update

---
+++ lista de numeros.py
@@ -0,0 +1,3 @@
+numeros = [1,2,3,4,5]
+print("la lista de numeros es : ",numeros)
+print("el primer y ultimo numero son : ",numeros[0],"y",numeros[4])
---
+++ numero mayor menor e igual.py
@@ -0,0 +1,11 @@
+a = int(input("escriba el primer numero:  "))
+b = int (input("escribal el segundo numero:  "))
+
+if a > b:
+    print(a," es el mayor")
+
+if a < b:
+    print(b, " es el mayor")
+
+if a == b:
+ print("los dos numeros son iguales")
